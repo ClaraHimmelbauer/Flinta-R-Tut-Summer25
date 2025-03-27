5e25e9bfff70bb740a84abc429da09cafca995be
ClaraHimmelbauer <clara.c.himmelbauer@gmail.com>
2025-03-27 10:55:31 +0100

    code für unit 1 gut kommentieren und hochladen

--- analysis/unit1.R
+++ analysis/unit1.R
@@ -1,7 +1,11 @@
-# FLINA Rtut UNIT 1 ---------------------------------------
-# FLINTA R tut line 2 #####################################
+# FLINA Rtut UNIT 1 -----------------------------------------------------------
+# FLINTA R tut line 2 #########################################################
+# the two lines above are an example on how to make a heading.
+# R-studio recognizes it and you can use this to navigate your code
+# the naviagation is at the bottom of the script sheet.
 
-# USE R AS A CALCULATOR -----------------------------------
+# 1. USE R AS A CALCULATOR ----------------------------------------------------
+# You can run your code by pressing Ctrl + Enter
 1 + 2
 
 # whitespaces do not matter
@@ -12,15 +16,21 @@
 6 /
   2
 
-# check equalities
+# you can use R to check equalities
 1 == 1       # 1 equals 1
 4 == 2 * 2   # 4 equals two times two
 4 > 5
 
 
-# ASSIGNING VARIABLES -------------------------------------
+# 2. ASSIGNING VARIABLES ------------------------------------------------------
+# assign a value to a variable by using <- or =
 a <- 4
 b = 5
+
+# you can also assign longer an more complex lines of codes
+# here, it's just a simple calcualtion
+# but the lines right to the "<-" can get pretty long
+b <- 2 + 3
 
 # print them = displaying the value
 a
@@ -33,20 +43,34 @@ a * b
 A
 a
 
-# OTHER DATA TYPES ----------------------------------------
+# 3. DATA TYPES ---------------------------------------------------------------
+# There are many data types. Here, I show you just the 3 most basic ones.
+# you will encounter more data types as you continue your R-journey.
+
+# numerics ------------------------------------------------
+# a is a numeric. (because a <- 4 and 4 is a number).
 class(a)
+# There are two sub-types of numerics: integers and doubles
+# integers are full number without decimal marks and they take up less memory and storage space
 
 # built in R help
+# by typing a question mark followed by a function you can access the built-in help page for this function
 ?class
 
-# character
+# character -----------------------------------------------
+# characters is text. in other programming languages it's often called "string"
+# characters in R are indicated by quotation marks or apostrophes
 c <- "this is a character"
 d <- 'hey'
 class(c)
 class(d)
+
+# you cannot do mathematical operations with text
 c + d
 
-# logical
+# logical -------------------------------------------------
+# there are 3 logicals: TRUE, FALSE, and NA. 
+# other programming languages might call this "boolean".
 d <- TRUE
 e <- T
 f <- FALSE
@@ -54,36 +78,51 @@ f <- F
 class(d)
 class(f)
 
-# assign operations
-g <- 2 *3
+
+# 4. SOME OTHER VARIABLE BASICS -----------------------------------------------
+
+# assigning operations to variables is possible -----------
+g <- 2 * 3
 h <- 2 > 4
 g
 h
 
-# DON'T DO THIS
-# assining on the right side
+# DON'T do this !!! ---------------------------------------
+
+# don't assign a variable to the right side
 2 -> i
-# using no whitespace
+# please use whitespace
 i<-2
 
 # using equal signs is okay-ish. but it's not common
 i = 2
 
-# REMOVE VARIABLIES ---------------------------------------
+# don't use umlauts, it will lead to encoding problems
+ä <- 30
+
+# remove variables ----------------------------------------
+# rm and remove are the same
 rm(a)
 remove(b)
+
+# you can remove multiple objects
 rm(c, d)
 
 # clean your whole environment
 rm(list = ls())
-# after that you can clear your cache
-# that's important for large datasets
-gc()   # garbage collect
+
+# after you delete something, you might want to clear your cache
+# the fucntion for this is gc() which stands for garbage collect
+gc()
+# this is especially improtant if you work with large data sets
+# which take up all of your PC's memory (RAM).
+# sometimes you need to gc() after each line (yeah, it's a pain in the ass)
 
 
-# VECTORS -------------------------------------------------
+# 5. VECTORS ------------------------------------------------------------------
 # numeric vector
 # c() stands for "combine"
+# in a vector, we combine multiple values into one object - the vector
 a <- c(2, 3, 5, 6, 10, -1, 2.4)
 a
 class(a)
@@ -93,23 +132,26 @@ b <- c("a", "hello", "evening", "ciao")
 class(b)
 
 
-# exercise
+# exercise ------------------------------------------------
 # make a vector that has both strings and numerics in it
-# what happens?
-# what class does it have?
+# what happens? all objects are cahracter
+# what class does it have? character. a vector can only have one data type
 c <- c("hello", "2", 4, "no")
 c
-class("4")
+class(c)
 
 # convert it to numeric
 d <- as.numeric(c)
-class(d)
 d
+class(d)
+# text cannot be converted to numerics. therefore, NAs are created.
 
-# MATRIX --------------------------------------------------
+# 6. MATRIX -------------------------------------------------------------------
 # before that clean environment
-rm(list = ls())
+rm(list = ls()); gc()
 
+# create a matrix -----------------------------------------
+# we want to create a matrix with thre columns.
 # assign 3 vectors, all of them have a length of 10
 # the first vector should be an ID of a person
 # the second vector the age
@@ -127,21 +169,27 @@ class(BOOKS)
 colnames(BOOKS) <- c("id", "age", "books")
 BOOKS
 
-# ACCESS DIFFERENT ELEMENTS OF A VECTOR -------------------
+# 7. ACCESS ELEMENTS ----------------------------------------------------------
+# vector --------------------------------------------------
 b
 # access 3rd element of vector
 b[3]
 # acess multiple arguments
 b[c(3, 5)]
 
-# ACCESSING A MATRIX --------------------------------------
+# matrix --------------------------------------------------
 BOOKS
 # element of 5th row and 2nd column
 BOOKS[5, 2]
 BOOKS[c(5, 7), 2]
 
 
-# DATAFRAME -----------------------------------------------
+# 8. DATAFRAME ----------------------------------------------------------------
+# dataframes are what makes R cool to work with
+# it's like a table where each column stores another variable
+# the columns can have different data types (which is not the case for matrices)
+
+# dataframe basics ----------------------------------------
 df <- data.frame(BOOKS)
 df
 # look at it by calling the View function (with a captial V)
@@ -149,18 +197,23 @@ View(df)
 
 # accessing columns 
 df$age
+# the dataframe has two dimensions. the first dimension is the row, the second one the columns
+# therefore, when we use cornered brackets to access a dataframe, we need to call both dimensions
+# we want all rows for the column called "age".
+# by writing nothing in front of the comma, we tell R, that we want to access all rows
+df[, "age"]
 
 # age column is the same as vector b
 df$age == b
 table(df$age == b)
 
-# new variable = new column
+# new columns/variables -----------------------------------
 # ifelse(condition, if, else)
 # if a person is 60 or older, "yes" that person is retired, else "no" the person is not retired
 df$retired <- ifelse(df$age > 60, "yes", "no")
 df
 
-# add rows
+# add rows - more complex. this is just an example
 # df_2 <- data.frame("id" = 11,
 #          "age" = 20,
 #          "books" = 13)
@@ -168,7 +221,7 @@ df
 # df_3 <- dplyr::bind_rows(df, df_2)
 # df_3$retired <- ifelse(df_3$age > 60, "yes", "no")
 
-# FUNCTION ------------------------------------------------
+# 9. FUNCTIONS ----------------------------------------------------------------
 # here we want to write a function that assigns "yes"
 # if a person is older than 60 and "no" otherwise
 
@@ -190,6 +243,8 @@ retired(61)
 
 # try the function on our vector
 retired(df$age)
+# this does not work because or funciton only works on one element (x)
+# however, the vector x has a length of 10 and therefore 10 elements
 
 # LOOP over or vector -------------------------------------
 # for i (= each element) in the vector df$age
@@ -199,7 +254,7 @@ for(i in df$age){
   print(x)
 }
 
-# How to use our custom function on the datafame
+# How to use our custom function on the datafame ----------
 # if it's overwhelming it's ok
 
 # way 1: loop
@@ -217,3 +272,4 @@ df
 # or sapply for simple apply
 # the apply function is there to "vectorize" operations
 df$retired3 <- sapply(df$age, retired)
+df
