4199dcc25f9a517125c2d7d48bd3f3be7792a146
ClaraHimmelbauer <clara.c.himmelbauer@gmail.com>
2025-05-19 13:47:18 +0200

    continue preparing tomorrow's session

--- analysis/unit6_prep.R
+++ analysis/unit6_prep.R
@@ -2,7 +2,7 @@
 # PREP ----------------------------------------------------
 rm(list = ls()); gc()
 
-packages <- c("tidyverse", "lmtest", "haven", "ggridges")
+packages <- c("tidyverse", "lmtest", "haven", "ggridges", "broom")
 sapply(packages, library, character.only = T)
 
 # DATA ----------------------------------------------------
@@ -104,3 +104,105 @@ ols <- lm(inc20 ~ degurba +
           weights = wght,
           data = silc)
 summary(ols)
+
+# NICE TABLES ---------------------------------------------
+
+# the classics: stargazer
+stargazer::stargazer(ols)
+stargazer::stargazer(ols, type = "text")
+
+# the modern way: modelsummary which works for word as well
+modelsummary::modelsummary(ols)
+
+# to comapre models: texreg
+texreg::screenreg(ols)
+
+
+# THE COEFFICIENT PLOT ------------------------------------
+
+dfols <- broom::tidy(ols, conf.int = T)
+dfols$sig <- ifelse(dfols$p.value < 0.01, "1% level",
+                    ifelse(dfols$p.value < 0.05, "5% level",
+                           ifelse(dfols$p.value < 0.1, "10% level", "not significant")))
+
+ggplot(dfols, aes(x = term, y = estimate, color = sig)) +
+  geom_point() +
+  geom_errorbar(aes(ymin = conf.low, ymax = conf.high), width = 0.2) +
+  geom_hline(yintercept = 0) +
+  coord_flip() +
+  labs(title = "OLS Coefficients",
+       x = "Variable",
+       y = "Coefficient") +
+  scale_color_manual(name = "Significance level", 
+                     values = c("slateblue4", "plum", "grey")) +
+  theme_minimal()
+
+# ASSESSING OVERALL MODEL FIT -----------------------------
+
+# f-Statistik, and rsquared already in there
+summary(ols)
+
+# comparing multiple models
+AIC(ols); BIC(ols)
+
+# LINEARITY -----------------------------------------------
+# linearity is violated
+
+lin <- data.frame(residuals = residuals(ols), fitted = fitted(ols))
+ggplot(lin, aes(x = fitted, y = residuals)) +
+  geom_point() +
+  geom_smooth(color = "#5a78c8") +
+  theme_minimal() +
+  ylab("Residuals") +
+  xlab("Fitted values") +
+  labs(title = "Linearity of model")
+
+# we want no systematic pattern between fitted values and residuals.
+### linear pattern: omitted variable, misspecification, variable that entered wrongly
+### u-shape/curved pattern: nonlinearity between in the relationship between predictors and the outcome
+### fan shape: heteroskedasticity
+# i want randomness, and no pattern in the residuals vs fitted values
+
+# so we have a biased model in essence.
+# ols assumes linear connection. if the true relationship were quadratic, then we would get biased estimates
+
+
+# HETEROSKEDASTICITY --------------------------------------
+
+# already see graph
+
+# we have heteroskedasticity
+lmtest::bptest(ols)
+
+# general note on tests: they nearly always fail. so graphical representation is important
+
+# NORMALITY -----------------------------------------------
+ggplot(lin, aes(x = residuals)) +
+  geom_histogram(aes(y = ..density..), fill = "grey") +
+  geom_density(color = "#5a78c8", linewidth = 2) +
+  stat_function(fun = dnorm, 
+                args = list(mean = 0,
+                            sd = sd(lin$residuals)),
+                color = "#4d4e4d", linetype = "dashed", size = 1.5) +
+  theme_minimal() +
+  labs(title = "Distribution of residuals")
+
+qqnorm(residuals(ols)); qqline(residuals(ols))
+shapiro.test(residuals(ols))
+
+# AUTOCORRELATION OF RESIDUALS ----------------------------
+# leads to inefficiency
+
+lmtest::bgtest(ols, order = 1)
+
+# MULTICOLLINEARITY --------------------------------------
+# to be expected
+# variables shouldn't be interpreted independently
+car::vif(ols)
+
+# INFLUENCE -----------------------------------------------
+plot(cooks.distance(ols))
+
+# nothing too bad, but we could exclude this one observation
+
+# BOOTSTRAPPING STANDARD ERRORS ---------------------------
